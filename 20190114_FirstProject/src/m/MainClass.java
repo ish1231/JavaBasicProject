4d0325fe60095e6a763e3e0411765b8a4b1bb3dc
ish <ish@DESKTOP-JR7VBKE>
2019-02-08 14:33:05 +0900

    bbb

---
+++ 20190114_FirstProject/src/m/MainClass.java
@@ -0,0 +1,48 @@
+package m;
+/*
+ * 1. 자바언어
+ *   1) 변수(한 개의 데이터 저장)
+ *   2) 저장 공간 크기 설정(기본데이터형 - 자바에서 지원)
+ *        정수형: byte (1byte => 8bit) 
+ *              short(2byte => 16bit) - c언어 호환
+ *              int  (4byte => 32bit)
+ *              long (8byte => 64bit) - 금융권
+ *        실수형: float(4byte), double(8byte)
+ *        문자형: char(2byte)
+ *             1byte: 싱글바이트(아스키코드 => c)
+ *             2byte: 멀티바이트(Unicode => Java) - 모든 언어의 호환
+ *        논리형: boolean(1byte) / true / false
+ *   =========================================================== 데이터 저장
+ *   3) 연산자
+ *   4) 제어문★★★★★
+ *   =========================================================== 데이터 제어
+ *   5) 여러 개 데이터를 한 번에 저장 => 배열
+ *   6) 객체지향 프로그램: 재사용 => 조립을 하는 프로그램★★★★★
+ *   7) 예외처리
+ *   8) 라이브러리 => 자바에서 제공하는 기능
+ *        =======
+ *      컬렉션, 쓰레드, IO, 네트워크, JDBC
+ *   9) 조립연습★★★★★
+ *   ============================================================
+ *   분석    
+*/
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+
+	//	System.out.println("");
+		
+		
+		  System.out.println("Hello Java!!"); 
+		  System.out.println("Hello Java!!");
+		  System.out.println("Hello Java!!"); 
+		  System.out.println("Hello Java!!");
+		  System.out.println("Hello Java!!");
+		  
+		  MainClass m = new MainClass();
+		  		
+		
+	}//main() end
+
+}//class end
